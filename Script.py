fec2c5c6f41c54c4004ae75038527acd27823c0b
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2023-12-21 20:26:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -146,7 +146,7 @@ my features Stay here new features coming soon...
 
 ❤️ Powered By : @Tr_LinksZz ❤️</b>"""
 
-    SUPPORT_TXT = """<b> Join Our My Official Channel's & Group's. </b>"""
+    SUPPORTING_TXT = """<b> Join Our My Official Channel's & Group's. </b>"""
 
     CREDITS_TXT = """<b> My Owner Add Soon, Bot & Channel Maintains Work Admin Also busy Add Soon </b>"""
     
