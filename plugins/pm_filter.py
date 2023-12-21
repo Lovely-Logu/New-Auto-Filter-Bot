0e906cbc8f7f15d33edc70661a1cfcf9114e8e2d
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2023-12-21 22:58:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1441,7 +1441,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         InputMediaPhoto(random.choice(PICS))
     )
     await query.message.edit_text(
-        text=script.SUPRT_TXT,
+        text=script.SUPRT_TXT.format(temp.B_NAME)
         reply_markup=reply_markup,
         parse_mode=enums.ParseMode.HTML
        )
