0a1271d22b2b1aaf120ba4ddb52f439c39e0bf55
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2023-12-30 16:59:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,15 +43,15 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('߷ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ߷', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🦋 Cʜᴀɴɴᴇʟ 🦋', url='https://t.me/Tr_LinksZz'),
-                    InlineKeyboardButton('✨ Mᴏᴠɪᴇ Gʀᴏᴜᴘ ✨', url="https://t.me/Discussion_tr_links")
+                    InlineKeyboardButton('★ 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url='https://t.me/Tr_LinksZz'),
+                    InlineKeyboardButton('♧︎︎︎ 𝖬𝗈𝗏𝗂𝖾 𝖦𝗋𝗈𝗎𝗉', url="https://t.me/Discussion_tr_links")
                 ],[
-                    InlineKeyboardButton('👻 Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('👾 Aʙᴏᴜᴛ', callback_data='about') 
+                    InlineKeyboardButton('ꕥ 𝖧𝖾𝗅𝗉', callback_data='help'),
+                    InlineKeyboardButton('♕︎ 𝖠𝖻𝗈𝗎𝗍', callback_data='about') 
                 ],[
-                    InlineKeyboardButton('🆕 Support 🆕', callback_data='suprt_info') 
+                    InlineKeyboardButton('𖤍 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𖤍', callback_data='suprt_info') 
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_text("👀")
@@ -97,15 +97,15 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('߷ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ߷', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🦋 Cʜᴀɴɴᴇʟ 🦋', url='https://t.me/Tr_LinksZz'),
-                    InlineKeyboardButton('✨ Mᴏᴠɪᴇ Gʀᴏᴜᴘ ✨', url="https://t.me/Discussion_tr_links")
+                    InlineKeyboardButton('★ 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url='https://t.me/Tr_LinksZz'),
+                    InlineKeyboardButton('♧︎︎︎ 𝖬𝗈𝗏𝗂𝖾 𝖦𝗋𝗈𝗎𝗉', url="https://t.me/Discussion_tr_links")
                 ],[
-                    InlineKeyboardButton('👻 Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('👾 Aʙᴏᴜᴛ', callback_data='about') 
+                    InlineKeyboardButton('ꕥ 𝖧𝖾𝗅𝗉', callback_data='help'),
+                    InlineKeyboardButton('♕︎ 𝖠𝖻𝗈𝗎𝗍', callback_data='about') 
                 ],[
-                    InlineKeyboardButton('🆕 Support 🆕', callback_data='suprt_info') 
+                    InlineKeyboardButton('𖤍 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𖤍', callback_data='suprt_info') 
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
